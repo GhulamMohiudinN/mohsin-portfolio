a62f39d42871c144fe7fb5a187ff137fcaa826a6
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-06-06 18:52:23 +0500

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2015 - 2019",
+    institution: "Gift University of Lahore",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary Certificate",
+    duration: "2013 - 2015",
+    institution: "District Public School Okara",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2002 - 2013",
+    institution: "Hujra Shah Muqeen School",
+  },
+];
---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "MUHSAN SATTAR",
+  profile: "/profile.jpg",
+  designation: "Software Developer",
+  description:
+    "My name is MUHSAN SATTAR. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "muhsansattar97@gmail.com",
+  phone: "+923049239011",
+  address: "OKARA, Punjab, Pakistan",
+  github: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  facebook: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  linkedIn: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  twitter: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  stackOverflow: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  leetcode: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  devUsername: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+  resume: "https://www.upwork.com/freelancers/~01b8262a0ffa2f4860",
+};
